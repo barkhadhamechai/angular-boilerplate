70746a7f64ca20c74f4d907d436391011edd91ab
barkhadhamechai <barkhadhamechai@gmail.com>
2019-04-29 12:15:27 +0530

    added ngrx

---
+++ angular/src/store/state/app.state.ts
@@ -0,0 +1,13 @@
+import { RouterReducerState } from '@ngrx/router-store';
+
+export interface IAppState {
+    router?: RouterReducerState;
+}
+
+export const initialAppState: IAppState = {
+
+}
+
+export function getInitialState(): IAppState {
+    return initialAppState;
+}
